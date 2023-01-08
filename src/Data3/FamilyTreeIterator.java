197efd9526d4af6f0b5b9235b6a907c346c444be
Makkyro <70462690+Makkyro@users.noreply.github.com>
2023-01-08 09:55:06 +0300

    Version_3.2

---
+++ src/Data3/FamilyTreeIterator.java
@@ -0,0 +1,25 @@
+﻿package Data3;
+
+import java.util.Iterator;
+import java.util.List;
+
+    
+public class FamilyTreeIterator implements Iterator<Human> {
+
+    private List<Human> humanList;
+    private int index;
+    
+    public FamilyTreeIterator(List<Human> humanList) {
+        this.humanList = humanList;
+    }
+
+    @Override
+    public boolean hasNext() {
+        return index < humanList.size();
+    }
+
+    @Override
+    public Human next() {
+        return humanList.get(index++);
+    }
+}
